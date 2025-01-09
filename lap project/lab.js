273220700d00a8814cfe67aa9c241f582b14a2f8
pardeepsingh5460 <pardeepsingh5460@gmail.com>
2025-01-09 18:42:37 +0530

    Add files via upload

---
+++ lap project/lab.js
@@ -0,0 +1,9 @@
+function learnMore() {
+    alert("Discover our world-class chemistry lab services!");
+  }
+  
+  document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We'll get back to you soon.");
+  });
+  
